341919a7548e8f7f07f63d6b0064f99de83b97d1
Brian Bernal <bbernalc93@gmail.com>
2023-02-01 00:22:11 -0500

    Se realiza la implementación de optimizar las peticiones Http y se incluyen animaciones de CSS

--- src/app/pais/services/pais.service.ts
+++ src/app/pais/services/pais.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Country } from '../interfaces/pais.interface';
@@ -10,26 +10,31 @@ import { Capital } from '../interfaces/capital.interface';
 })
 export class PaisService {
 
-  private apiUrl:string = 'https://restcountries.com/v2';
-  constructor(private http:HttpClient) { }
+  private apiUrl: string = 'https://restcountries.com/v2';
+  get httParams() {
+    return new HttpParams()
+      .set('fields', 'name,capital,alpha2Code,flag,population')
+  }
 
-  BuscarPais(termino:string):Observable<Country[]>{
+  constructor(private http: HttpClient) { }
+
+  BuscarPais(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/name/${termino}`;
-    return this.http.get<Country[]>(url);
+    return this.http.get<Country[]>(url, {params:this.httParams});
   }
 
-  BuscarCapital(termino:string):Observable<Country[]>{  
+  BuscarCapital(termino: string): Observable<Country[]> {
     const url = `${this.apiUrl}/capital/${termino}`;
-    return this.http.get<Capital[]>(url);
+    return this.http.get<Capital[]>(url,{params:this.httParams});
   }
 
-  getPaisPorAlpha(id:string):Observable<Country>{
-    const url = `${this.apiUrl}/alpha/${ id }`;
+  getPaisPorAlpha(id: string): Observable<Country> {
+    const url = `${this.apiUrl}/alpha/${id}`;
     return this.http.get<Country>(url);
   }
 
-  BuscarRegion(region:string):Observable<Country[]>{
-    const url = `${this.apiUrl}/regionalbloc/${ region }`;
-    return this.http.get<Country[]>(url);
+  BuscarRegion(region: string): Observable<Country[]> {
+    const url = `${this.apiUrl}/regionalbloc/${region}`;
+    return this.http.get<Country[]>(url, { params:this.httParams });
   }
 }
